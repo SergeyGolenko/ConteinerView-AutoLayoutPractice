4ebd193ed48b20a1b5c8601641200f0d46cdadc7
SergeyGolenko <1989sergiotay@gmail.com>
2021-02-11 11:03:27 +0200

    Initial Commit

---
+++ ConteinerView,AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConteinerView,AutoLayoutPractice
+//
+//  Created by Сергей Голенко on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
